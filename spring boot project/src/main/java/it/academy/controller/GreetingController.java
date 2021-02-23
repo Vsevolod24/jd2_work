71271f7a473a16ddcff4bc465d37616154fea288
Vsevolod <vsevolodkucko@gmail.com>
2021-02-23 21:12:13 +0300

    Project

---
+++ spring boot project/src/main/java/it/academy/controller/GreetingController.java
@@ -0,0 +1,15 @@
+package it.academy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+    @GetMapping("/")
+    public String home ( Model model) {
+        return "home";
+    }
+}
